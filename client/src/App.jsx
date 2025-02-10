b6ac6d8b206559b9bf8369f562010276cfd38149
juri turiano <juricuber@gmail.com>
2025-02-11 03:05:32 +0800

    added components - download library - added logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/landingPage";
+import Header from "./components/header";
+
 function App() {
-  return <></>;
+  return (
+    <div className="bg-primary font-roboto  h-dvh  text-white">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
